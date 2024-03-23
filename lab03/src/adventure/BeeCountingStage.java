e634db2a9eec4b697a41aa1a8a246a49a86db8d8
Wang JiaYuan <jy_bmfs@163.com>
2024-03-23 10:40:51 +0800

    Finish BeeCountingStage of lab03.

--- lab03/src/adventure/BeeCountingStage.java
+++ lab03/src/adventure/BeeCountingStage.java
@@ -3,6 +3,7 @@ package adventure;
 import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.StdRandom;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -16,6 +17,8 @@ public class BeeCountingStage implements AdventureStage {
     public BeeCountingStage(In in) {
         this.in = in;
         this.responses = Map.of("go", new SpeciesListStage(in));
+        this.input = new ArrayList<String>();
+//        this.input = new DLList<String>();
     }
 
     /**
@@ -83,7 +86,7 @@ public class BeeCountingStage implements AdventureStage {
      */
     private int sumInput() {
         int sum = 0;
-        for (int i = 0; i <= this.input.size(); i++) {
+        for (int i = 0; i < this.input.size(); i++) {
             sum += Integer.parseInt(this.input.get(i));
         }
         return sum;
